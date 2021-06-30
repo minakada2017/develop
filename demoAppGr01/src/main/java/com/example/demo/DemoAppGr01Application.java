db506a574e9c9195d4ca07e1677a9ce858de6b39
DESKTOP-5CN5L2H\中田光昭 <minakada2012@gmail.com>
2021-06-30 17:40:23 +0900

    新規作成

---
+++ demoAppGr01/src/main/java/com/example/demo/DemoAppGr01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppGr01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppGr01Application.class, args);
+	}
+
+}
